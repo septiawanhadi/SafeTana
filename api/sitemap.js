9811cdfd79dc83efebd107b38baa5044bcd2c2df
Septiawan Hadi <sptwn26@upi.edu>
2026-03-17 14:34:33 +0700

    fix: final optimized sitemap api

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,7 +1,4 @@
 export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/xml; charset=utf-8');
-  res.setHeader('Cache-Control', 'public, max-age=86400, s-maxage=86400');
-
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
@@ -70,7 +67,10 @@ export default function handler(req, res) {
     <changefreq>monthly</changefreq>
     <priority>0.5</priority>
   </url>
-</urlset>`;
+</urlset>`.trim();
 
+  res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+  res.setHeader('Cache-Control', 'public, max-age=86400, s-maxage=86400');
+  res.setHeader('X-Robots-Tag', 'noindex');
   res.status(200).send(sitemap);
 }
